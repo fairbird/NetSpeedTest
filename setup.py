794b846ce6e773522c7c610b11f2d688f74509f0
RAED <rrrr53@hotmail.com>
2019-07-15 02:21:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from distutils.core import setup
-import setup_translate
 
 pkg = 'SystemPlugins.NetSpeedTest'
 setup (name = 'enigma2-plugin-systemplugins-netspeedtest',
@@ -8,5 +7,4 @@ setup (name = 'enigma2-plugin-systemplugins-netspeedtest',
        packages = [pkg],
        package_dir = {pkg: 'plugin'},
        package_data = {pkg: ['plugin.png', '*/*.png', 'locale/*/LC_MESSAGES/*.mo']},
-       cmdclass = setup_translate.cmdclass, # for translation
       )
